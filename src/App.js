7d9238083687ea532f48cda39e7391ebb49151e4
shamimgazi8 <shamim.g47@hotmail.com>
2023-11-15 01:57:14 +0600

    change FAKE API

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ export default function App() {
   }
   const totalpoints = state.questions.reduce((pre, crr) => pre + crr.points, 0);
   useEffect(function () {
-    fetch("http://localhost:8000/questions")
+    fetch("https://json.extendsclass.com/bin/170e25009d57")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "reciveData", payload: data }))
       .catch((err) => dispatch({ type: "error" }));
